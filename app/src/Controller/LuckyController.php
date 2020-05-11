5ad5f88d3a3b757753abd52d6ffe258df1406394
18_zebala <maciej.zebala@student.uj.edu.pl>
2020-05-11 13:58:42 +0200

    vendor

--- app/src/Controller/LuckyController.php
+++ app/src/Controller/LuckyController.php
@@ -7,14 +7,15 @@ namespace App\Controller;
 // ...
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
  * Class LuckyController.
  */
-class LuckyController
+class LuckyController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/liczba")
      */
 
     public function number(): Response
