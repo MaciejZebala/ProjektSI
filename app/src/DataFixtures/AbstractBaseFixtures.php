adfde5db00f018ae57752048104205ac217eab4a
Maciej Zębala <kropka551@op.pl>
2020-05-16 14:23:37 +0200

    Modify AbstractBaseFixtures

--- app/src/DataFixtures/AbstractBaseFixtures.php
+++ app/src/DataFixtures/AbstractBaseFixtures.php
@@ -1,17 +1,52 @@
 <?php
 
+/**
+ * Base fixtures.
+ */
+
 namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
-class AbstractBaseFixtures extends Fixture
+
+/**
+ * Class AbstractBaseFixtures.
+ */
+
+abstract class AbstractBaseFixtures extends Fixture
 {
-    public function load(ObjectManager $manager)
-    {
-        // $product = new Product();
-        // $manager->persist($product);
+    /**
+     * Faker.
+     *
+     * @var \Faker\Generator
+     */
+    protected $faker;
 
-        $manager->flush();
+    /**
+     * Persistence object manager.
+     *
+     * @var \Doctrine\Persistence\ObjectManager
+     */
+    protected $manager;
+
+    /**
+     * Load.
+     *
+     * @param \Doctrine\Persistence\ObjectManager $manager Persistence object manager
+     */
+    public function load(ObjectManager $manager): void
+    {
+        $this->manager = $manager;
+        $this->faker = Factory::create();
+        $this->loadData($manager);
     }
+
+    /**
+     * Load data.
+     *
+     * @param \Doctrine\Persistence\ObjectManager $manager Persistence object manager
+     */
+    abstract protected function loadData(ObjectManager $manager): void;
 }
