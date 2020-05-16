d46d071be1d1ecfee1eb49edf66b5c1f8efdb914
Maciej Zębala <kropka551@op.pl>
2020-05-16 13:45:05 +0200

    Make Event entity and add special comment in entity

---
+++ app/src/Entity/Event.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Entity;
+
+use DateTimeInterface;
+use App\Repository\EventRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=EventRepository::class)
+ * @ORM\Table("events")
+ */
+class Event
+{
+    /**
+     * Primary key
+     *
+     * @var int
+     *
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * Date
+     *
+     * @var DateTimeInterface
+     *
+     * @ORM\Column(type="datetime")
+     */
+    private $date;
+
+    /**
+     * Title
+     *
+     * @var string
+     *
+     * @ORM\Column(type="string", length=70)
+     */
+    private $title;
+
+    /**
+     * Getter for Id
+     *
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Getter for Date
+     *
+     * @return DateTimeInterface|null
+     */
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    /**
+     * Setter for Date
+     *
+     * @param DateTimeInterface $date Date
+     */
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Getter for Title
+     *
+     * @return string|null
+     */
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    /**
+     * Setter for Title
+     *
+     * @param string $title Title
+     */
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+}
