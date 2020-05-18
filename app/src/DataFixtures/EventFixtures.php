155bad51679805e56434f17c6ea5922b2e3c0d22
Maciej Zębala <kropka551@op.pl>
2020-05-18 16:07:55 +0200

    Add categoryFixtures and functions at abstractFixtures and EventFixtures

--- app/src/DataFixtures/EventFixtures.php
+++ app/src/DataFixtures/EventFixtures.php
@@ -3,12 +3,13 @@
 namespace App\DataFixtures;
 
 use App\Entity\Event;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
 /**
  * Class EventFixtures.
  */
-class EventFixtures extends AbstractBaseFixtures
+class EventFixtures extends AbstractBaseFixtures implements DependentFixtureInterface
 {
     /**
      * Load data.
@@ -18,12 +19,25 @@ class EventFixtures extends AbstractBaseFixtures
 
     public function loadData(ObjectManager $manager): void
     {
-        for ($i = 0; $i < 10; ++$i) {
+        $this->createMany(10, 'events', function ($i) {
             $event = new Event();
             $event->setTitle($this->faker->sentence);
             $event->setDate($this->faker->dateTimeBetween('-100 days', '-1 days'));
-            $this->manager->persist($event);
-        }
+            $event->setCategory($this->getRandomReference('categories'));
+            return $event;
+        });
+
         $manager->flush();
+    }
+
+    /**
+     * This method must return an array of fixtures classes
+     * on which the implementing class depends on.
+     *
+     * @return array Array of dependencies
+     */
+    public function getDependencies()
+    {
+        return [CategoryFixtures::class];
     }
 }
